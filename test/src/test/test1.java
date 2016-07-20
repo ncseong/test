4027d5f0a422de10d07f803bccd9be6a89ce6bb8
cheol <cheol@DESKTOP-MFKQBN5>
2016-07-20 15:22:51 +0900

    ttt

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -5,7 +5,7 @@ import com.supermap.data.WorkspaceConnectionInfo;
 public class test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub22
 		WorkspaceConnectionInfo info = new WorkspaceConnectionInfo();
 	}
 
