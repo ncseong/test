5de59a9cc5605c6266e84e13a4f5735e9e1680d2
cheol <cheol@DESKTOP-MFKQBN5>
2016-07-27 18:26:25 +0900

    8

---
+++ test/src/test/test2.java
@@ -0,0 +1,13 @@
+package test;
+
+import com.supermap.data.Workspace;
+import com.supermap.data.WorkspaceConnectionInfo;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WorkspaceConnectionInfo work = new WorkspaceConnectionInfo();
+	}
+
+}
